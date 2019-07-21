b0a9b46f47716abfc65aa83f7506c6d8b66825c5
Cody Walker <themightycoder@gmail.com>
2019-07-20 21:36:11 -0700

    working on GUI.... i know its sloppy and not MVC but gimme a break I'm just experimenting!

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,7 @@
 package client;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -8,7 +10,16 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class Client {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+public class Client implements ActionListener {
 
 	private final static int PORT = 36979;
 
@@ -19,7 +30,37 @@ public class Client {
 	private static DataOutputStream dos;
 	private static Thread sendMessage, receiveMessage;
 
-	public static void main(String[] args) {
+	private JPanel middlePanel;
+	private JTextArea display;
+	private JScrollPane scroll;
+	private JTextField inputText;
+	private JFrame frame;
+
+	public Client() {
+
+		middlePanel = new JPanel();
+		middlePanel.setBorder(new TitledBorder(new EtchedBorder(), "Display Area"));
+
+		// create the middle panel components
+		display = new JTextArea(22, 58);
+		display.setEditable(false); // set textArea non-editable
+		scroll = new JScrollPane(display);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+		// Add Textarea in to middle panel
+		middlePanel.add(scroll);
+
+		// Text input.
+		inputText = new JTextField(58);
+		inputText.addActionListener(this);
+		middlePanel.add(inputText);
+
+		// My code
+		frame = new JFrame();
+		frame.add(middlePanel);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 
 		scanner = new Scanner(System.in);
 
@@ -36,25 +77,25 @@ public class Client {
 			dos = new DataOutputStream(socket.getOutputStream());
 
 			// Thread for sending messages.
-			sendMessage = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					String message;
-					while (true) {
-						// Read in message to deliver to server.
-						message = scanner.nextLine();
-
-						try {
-							// Write message to output stream.
-							dos.writeUTF(message);
-						} catch (IOException e) {
-							System.err.println("ERROR:  COULD NOT SEND MESSAGE TO SERVER!");
-							e.printStackTrace();
-							System.exit(-1);
-						}
-					}
-				}
-			});
+//			sendMessage = new Thread(new Runnable() {
+//				@Override
+//				public void run() {
+//					String message;
+//					while (true) {
+//						// Read in message to deliver to server.
+//						message = inputText.getText();
+//
+//						try {
+//							// Write message to output stream.
+//							dos.writeUTF(message);
+//						} catch (IOException e) {
+//							System.err.println("ERROR:  COULD NOT SEND MESSAGE TO SERVER!");
+//							e.printStackTrace();
+//							System.exit(-1);
+//						}
+//					}
+//				}
+//			});
 
 			// Thread for receiving messages.
 			receiveMessage = new Thread(new Runnable() {
@@ -67,6 +108,7 @@ public class Client {
 							message = dis.readUTF();
 							// Display the message.
 							System.out.println(message);
+							display.append(message + "\n");
 						} catch (IOException e) {
 							System.err.println("ERROR:  COULD NOT RECEIVE MESSAGE!");
 							e.printStackTrace();
@@ -75,8 +117,8 @@ public class Client {
 					}
 				}
 			});
-			
-			sendMessage.start();
+
+//			sendMessage.start();
 			receiveMessage.start();
 
 		} catch (UnknownHostException e) {
@@ -88,6 +130,24 @@ public class Client {
 			e.printStackTrace();
 			System.exit(-1);
 		}
+
+	}
+
+	public static void main(String[] args) {
+
+		new Client();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		try {
+			dos.writeUTF(inputText.getText());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		inputText.setText("");
 	}
 
 }
